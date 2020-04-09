5f3c1c863bd7133971ca0c88be2dac6f3695aff3
Jonathan Bruce <jonathanbruce@Jonathans-MacBook-Air.local>
2020-04-08 18:56:38 -0700

    Updated port number

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8000
+const port = 3000
 
 app.post('/api/suggest', (req, res) => {
 	console.log(`Parameters: ${req.query.q}`)
