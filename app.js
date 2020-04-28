f680997df6567bc77e3f10c11faaa6d3d645f2a5
Jonathan Bruce <jonathan.m.bruce#@gmail.com>
2020-04-28 00:44:35 -0700

    Lots of changes, but this likely does not compile.  Basic logic present, but need to sleep on this

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+CustomElementRegistry fs = require('fs');
 const express = require('express')
 const app = express()
 const port = 3001
+const file = 'datamodel.json'
 
 app.post('/api/suggest', (req, res) => {
 	console.log(`Parameters: ${req.query.q}`)
@@ -16,7 +18,13 @@ app.post('/api/suggest2', (req, res) => {
 	if (req.query.q === null ) {
         	res.send('Invalid query')
 	} else {
-         res.send(suggestions)
+		 console.log(suggestions)
+		let reader = new FileReader()
+		reader.readAsText(file) // 
+		let jsonDataModel = JSON.stringify(reader)
+		let newString = userInput.newString().toLowerCase()
+		suggestions = parseInput(newString)
+		res.send(suggestions)
 	}
 	})
 
@@ -27,9 +35,49 @@ app.use(function (req, res, next) {
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
-const suggestions = [
- 	{ object: 'Account' },
-	{ object: 'Opportunity' },
-	{ object: 'Lead' },
-	{ object: 'Report' }
-];
+function parseInputAndTokenize(userInput) {
+	const fs.readFile('datamodel.json)')
+	let newString = userInput.newString().toLowerCase()
+	let token = ' '
+	tokenizedString = newString.split(token)  // thisis now an array
+	// now make a new array/class that classifies each tokern
+	for (int i=0; i<tokenizedString.length; i++) {
+	}
+	// recursive descent parser goes here
+}
+
+let suggestion = new String()
+
+function parseInput(userInputArray, index) {
+
+	if (entity(userInputArray[index] == '<Entity>') {
+		suggestion =+ ' <Entity> '
+		index++
+		return parseInput(userInputArray, index)
+	} else if (field(userInputArray[index] == '<Field>') {
+		suggestion =+ ' <Field> '
+		index ++
+		return parseInput(userInputArray, index)
+	} else {
+		suggestion =+ ' <EOF> '
+	} 
+	return suggestion;
+}
+// need a datstructure so I can match what I am seeing on each token 
+// with what I belive it to be 
+ function entity(token) {
+	if ((token == jsonDataModel.Opportunity) ||
+		(token == jsonDataModel.Account) ||
+		(token == jsonDataModel.Lead) ||
+		(token == jsonDataModel.Contact)) {
+			return '<Entity>'
+	}
+}
+
+function field(token) {
+	if ((token == jsonDataModel.Id) ||
+	(token == jsonDataModel.CreatedBy) ||
+	(token == jsonDataModel.ModifiedBy)) {
+		return '<Field>'
+	}
+}
